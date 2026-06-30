10b570f33851d19ebe2cd1f6959c7bc8da2ae0a0
rehanbussiness345 <rehanbussiness345@gmail.com>
2026-06-30 11:41:47 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,8 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteSingleFile()],
+  // 1. Move viteSingleFile() to the very end of the array
+  plugins: [react(), tailwindcss(), viteSingleFile()], 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
